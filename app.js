b786305d6e4cd98b0c56f4ee325957505cd2f3c0
xma1xma-maker <xma1xma@gmail.com>
2026-02-10 00:29:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const tg = window.Telegram.WebApp;
 tg.expand();
 
-const user = tg.initDataUnsafe.user;
+const user = tg.initDataUnsafe?.user;
 
 if (user) {
-  console.log("Telegram User:", user);
-  // لاحقًا نربط Firebase
-  document.getElementById("balance").innerText = "0.20 USDT";
+  document.getElementById("balance").innerHTML = "0.25 <small>USDT</small>";
 }
